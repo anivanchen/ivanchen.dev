6c227add5c156980277c9599cf3782170d215ce8
TheProgrammerOfLife <IvanChen07@gmail.com>
2021-08-08 20:41:06 -0400

    Made Text Size Responsive

--- src/components/OpeningScreen.js
+++ src/components/OpeningScreen.js
@@ -8,7 +8,7 @@ function OpeningScreen() {
           <ThemeToggle />
           <div className="opening-container">  
             <div className="text-container">
-              <h3>Hi, I'm <a className="link" href="https://github.com/anivanchen">Ivan Chen</a>, a current sophomore at <a className="link" href="https://stuy.enschool.org/">Stuyvesant High School</a>.</h3>
+              <h2>Hi, I'm <a className="link" href="https://github.com/anivanchen">Ivan Chen</a>, a current sophomore at <a className="link" href="https://stuy.enschool.org/">Stuyvesant High School</a>.</h2>
               <p>At Stuy, I am in the class of 2024. I enjoy computer science and mathematics. I love learning new things and starting new <a className="link" href="#gallery">projects</a>, especially those that challenge me and introduce novel topics. Learning never ends, and there's always more for me to explore.</p>
             </div>
             <a className="social-media-button dark" href="https://linkedin.com/in/ivan-chen-9b85481b6"><i class="bi bi-linkedin"></i></a>
