472914c33a7f0b4d34dd2d46b8fa68d707a86d27
TheProgrammerOfLife <IvanChen07@gmail.com>
2021-10-12 20:44:26 -0400

    Updated Other Projecys

--- components/OtherProjects.js
+++ components/OtherProjects.js
@@ -5,6 +5,12 @@ export default function OtherProjects() {
       <div className={styles.smallProjects}>
         <div className={styles.grid}>
           <div className={styles.smallProject}>
+            <a href="https://github.com/anivanchen/QR">
+              <p>QR →</p>
+              <p>A QR code generator and reader with encryption.</p>
+            </a>
+          </div>
+          <div className={styles.smallProject}>
             <a href="https://github.com/anivanchen/crypto">
               <p>crypto →</p>
               <p>A quick cryptography solution using crypto.</p>
@@ -14,6 +20,12 @@ export default function OtherProjects() {
             <a href="https://github.com/anivanchen/pycrypt">
               <p>pycrypt →</p>
               <p>A series of standalone scripts built with Cryptography for encryption and decryption of files / strings.</p>
+            </a>
+          </div>
+          <div className={styles.smallProject}>
+            <a href="https://github.com/anivanchen/todo">
+              <p>todo →</p>
+              <p>A simple todo app built with the PERN stack.</p>
             </a>
           </div>
           <div className={styles.smallProject}>
@@ -29,15 +41,15 @@ export default function OtherProjects() {
             </a>
           </div>
           <div className={styles.smallProject}>
-            <a href="https://github.com/anivanchen/QR">
-              <p>QR →</p>
-              <p>A QR code generator and reader with encryption.</p>
-            </a>
-          </div>
-          <div className={styles.smallProject}>
             <a href="https://github.com/anivanchen/morsecode">
               <p>morsecode →</p>
               <p>A Morse code encoder and decoder in Python.</p>
+            </a>
+          </div>
+          <div className={styles.smallProject}>
+            <a href="https://github.com/anivanchen/stuycs-classlist">
+              <p>stuycs-classlist →</p>
+              <p>A web scraper written in Python and uses BeautifulSoup to grab a list of student names and their respective periods.</p>
             </a>
           </div>
           <div className={styles.smallProject}>
