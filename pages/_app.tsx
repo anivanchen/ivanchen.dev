96d14b743a792fd90c036221827cd809e5a1e05a
anivanchen <IvanChen07@gmail.com>
2022-03-06 10:55:33 -0500

    Fix import typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/Head'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
