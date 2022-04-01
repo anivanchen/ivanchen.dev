3ef90f3cf6eec34d64da3f85f1f512c209b3bbe9
anivanchen <IvanChen07@gmail.com>
2022-04-01 10:31:04 -0400

    Update nonsus page

--- pages/giveaways.tsx
+++ pages/giveaways.tsx
@@ -20,11 +20,11 @@ const Giveaways: NextPage = () => {
       <main className={styles.main}>
         <div className={styles.mainContent}>
           <h1 id="fancy-text">Happy April Fools Day!</h1>
-          <h3>No free donut for you but here&apos;s a rick roll!</h3>
-          <video width="80%" loop autoPlay muted preload="auto" className={styles.centered}>
+          <h3>No free donut for you but here&apos;s a rickroll!</h3>
+          {/* <video width="80%" loop autoPlay muted preload="auto" className={styles.centered}>
             <source src={"/rick.mp4"} type="video/mp4" />
-          </video>
-          {/* <iframe data-v-8e62c10e="" id="video-frame" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&amp;enablejsapi=1&amp;autoplay=1&amp;mute=1" frameBorder="0" width="100%" height="480px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" style={{maxWidth: "720px"}} /> */}
+          </video> */}
+          <iframe data-v-8e62c10e="" id="video-frame" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&amp;enablejsapi=1&amp;autoplay=1&amp;mute=1" frameBorder="0" width="100%" height="480px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" style={{maxWidth: "720px"}} />
           <h3>Now don&apos;t spoil it for other people :3</h3>
         </div>
       </main>
