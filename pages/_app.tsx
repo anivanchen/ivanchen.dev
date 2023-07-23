9fb19b54f8788050c9f99d0c7a3c42c2ed342d21
Ivan Chen <IvanChen07@gmail.com>
2023-07-22 23:42:45 -0400

    Add vercel analytics (#5)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -34,6 +35,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
