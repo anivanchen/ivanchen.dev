6a89e6dc3549c3c94191be1e93bb1a9c7cf4d9f5
Ivan Chen <ivanchen07@gmail.com>
2024-07-24 03:23:12 -0400

    Update deprecated use of domains for remotePatterns

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/51217025'
+      }
+    ]
   },
   i18n: {
     locales: ["en"],
